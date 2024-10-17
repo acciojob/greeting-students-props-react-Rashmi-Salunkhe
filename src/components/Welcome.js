72a82a1bc36771c1ecdd8a1b2954a58ddea8d7e3
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-17 11:11:37 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,21 @@
-// write code for Welcome component here
+import React from 'react'
+
+
+
+const Welcome = ({ name }) => {
+
+    return (
+
+        <div>
+
+            <h1>Hey {name}!</h1>
+
+            <p><h2>Welcome to School.</h2></p>
+
+        </div>
+
+    )
+
+}
+
+export default Welcome;
